9271b3498ac41f86caea9aa7f65be86a49be0a36
Lelle222 <161458991+Lelle222@users.noreply.github.com>
2024-07-09 12:37:14 +0200

    feat: add delete appointment

--- src/lib/control-appointments.js
+++ src/lib/control-appointments.js
@@ -1,16 +1,19 @@
-import { createAppointment, deleteAppointment } from "./crud-appointment.js";
+import { deleteAppointment } from "./crud-appointments.js";
 
 document.addEventListener("DOMContentLoaded", () => {
   const deleteButtons = document.querySelectorAll(".delete-btn");
   deleteButtons.forEach((button) => {
-    button.addEventListener("click", () => {
-      if (window.confirm("Do you really want to delete your appointment?")) {
-        const id = button.dataset.row;
-        deleteAppointment(id);
-        const appointment = document.getElementById(id);
-        appointment.remove();
-      }
-    });
+    setDeleteRowBtn(button);
   });
-
 });
+
+function setDeleteRowBtn(btn) {
+  btn.addEventListener("click", () => {
+    if (window.confirm("Do you really want to delete your appointment?")) {
+      const id = btn.dataset.row;
+      deleteAppointment(id);
+      const appointment = document.getElementById(id);
+      appointment.remove();
+    }
+  });
+}
